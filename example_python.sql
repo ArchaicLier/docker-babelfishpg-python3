e03bdeeef804d76535c60bab76f39b3d692c54f8
1i3R <55849349+ArchaicLier@users.noreply.github.com>
2023-02-21 19:17:11 +0000

    Update example_python.sql

--- example_python.sql
+++ example_python.sql
@@ -1,6 +1,6 @@
 create extension if not exists plpython3u ;
 
-create function or replace hello_python()
+create or replace function hello_python()
 	returns setof text
 as $$
 	return('Hello','PostgreSQL','!')
