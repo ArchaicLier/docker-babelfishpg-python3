d7f0d09c0373603fe39cfa0a165d9fc29fda4bc7
1i3R <55849349+ArchaicLier@users.noreply.github.com>
2023-02-27 11:29:48 +0300

    test

--- start.sh
+++ start.sh
@@ -54,6 +54,8 @@ if [ ! -f ${BABELFISH_DATA}/postgresql.conf ]; then
 	./pg_ctl -D ${BABELFISH_DATA}/ stop
 fi
 
+echo -n MIGRATION_MODE;
+
 case MIGRATION_MODE in 
 	single-db) echo 'Server is running in singel-db mode';;
 	multi-db) echo 'Server is running in multi-db mode';;
